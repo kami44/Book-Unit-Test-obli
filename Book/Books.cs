77d2da8ad5532ee348756cfd4267ad69d8d1c679
Patrick Poul Nielsen <patr9244@edu.easj.dk>
2020-03-10 15:15:25 +0100

    Færdig med opgave 4 mangler 5

--- Book/Books.cs
+++ Book/Books.cs
@@ -4,10 +4,18 @@ namespace Book
 {
     public class Books
     {
-        private string _titel { get; set; }
+        private string _titel;
         private string _forfatter;
         private int _sidetal;
         private string _isbn13;
+
+        public Books() { }
+
+        public string Title
+        {
+            get => _titel;
+            set => _titel = value;
+        }
 
         public Books(string forfatter, int sidetal, string isbn13, string titel)
         {
@@ -17,12 +25,13 @@ namespace Book
             _titel = titel;
         }
 
+
         public string Forfatter
         {
             get => _forfatter;
             set
             {
-                if (value.Length > 2) throw new ArgumentException();
+                if (value.Length < 2) throw new ArgumentException();
                 _forfatter = value;
             }
         }
@@ -32,9 +41,8 @@ namespace Book
             get => _sidetal;
             set
             {
-                if (value > 4||value < 1000)
-                    if (value >= 4 && value <= 1000) throw new ArgumentOutOfRangeException();
-                _sidetal = value;
+                if (value >= 4 && value <= 1000) _sidetal = value;
+                else throw new ArgumentOutOfRangeException();
             }
         }
 
@@ -43,9 +51,14 @@ namespace Book
             get => _isbn13;
             set
             {
-                if (value.Length == 13) throw new ArgumentOutOfRangeException();
+                if (value.Length != 13) throw new ArgumentOutOfRangeException();
                     _isbn13 = value;
             }
         }
+
+        //public override string ToString()
+        //{
+        //    return _titel + _forfatter + _sidetal + _isbn13;
+        //}
     }
 }
